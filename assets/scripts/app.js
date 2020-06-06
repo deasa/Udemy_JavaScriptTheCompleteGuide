15db896d4d710c5dd84494ee48dafad4ba158774
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-06-06 08:41:37 -0600

    2.35

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(number1, number2) {
-  const result = number1 + number2;
-  alert(`The result is ${result}`);
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, calculationDescription);
 }
 
-add(1, 2);
-add(5, 5);
-
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener("click", add);
